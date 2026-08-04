2ba25a8bf4be5af0545226343e6148960865c6a5
walidatiyaai2025-gif <walidatiyaai2025@gmail.com>
2026-08-04 22:58:18 +0300

    i18n: default browser language to English

--- src/AIWordPressManager.Web/wwwroot/js/app-language.js
+++ src/AIWordPressManager.Web/wwwroot/js/app-language.js
@@ -1,9 +1,12 @@
 window.appLanguage = {
-  get: () => localStorage.getItem('aiwp-language') || 'ar',
+  get: () => localStorage.getItem('aiwp-language') || 'en',
   set: (value) => localStorage.setItem('aiwp-language', value),
   apply: (lang, dir) => {
-    document.documentElement.lang = lang;
-    document.documentElement.dir = dir;
-    document.body.dir = dir;
+    const culture = lang === 'ar' ? 'ar' : 'en';
+    const direction = culture === 'ar' ? 'rtl' : 'ltr';
+
+    document.documentElement.lang = culture;
+    document.documentElement.dir = direction;
+    document.body.dir = direction;
   }
 };
