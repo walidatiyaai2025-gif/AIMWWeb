385a8d1e9c3ce5f8633154c0312f22eb219056a8
walidatiyaai2025-gif <walidatiyaai2025@gmail.com>
2026-08-05 23:20:59 +0300

    Remove conflicting ReportsExports code-behind

--- src/AIWordPressManager.Web/Components/Pages/ReportsExports.razor.cs
+++
@@ -1,9 +0,0 @@
-namespace AIWordPressManager.Web.Components.Pages;
-
-public partial class ReportsExports : IDisposable
-{
-    void IDisposable.Dispose()
-    {
-        L.Changed -= Changed;
-    }
-}
