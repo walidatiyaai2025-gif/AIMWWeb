b3431f77c75c077f584118de2bc258806808f72b
walidatiyaai2025-gif <walidatiyaai2025@gmail.com>
2026-08-06 22:22:23 +0300

    feat: configure site ownership

--- src/AIWordPressManager.Persistence/Configurations/SiteConfiguration.cs
+++ src/AIWordPressManager.Persistence/Configurations/SiteConfiguration.cs
@@ -12,12 +12,14 @@ public sealed class SiteConfiguration : IEntityTypeConfiguration<Site>
         builder.ConfigureEntity();
         builder.Property(x => x.Name).HasMaxLength(200).IsRequired();
         builder.Property(x => x.SiteUrl).HasMaxLength(2048).IsRequired();
+        builder.Property(x => x.OwnerUserId);
         builder.Property(x => x.HomeUrl).HasMaxLength(2048);
         builder.Property(x => x.WordPressVersion).HasMaxLength(50);
         builder.Property(x => x.LanguageCode).HasMaxLength(20);
         builder.Property(x => x.ConnectionStatus).HasConversion<string>().HasMaxLength(40).IsRequired();
         builder.Property(x => x.IsDeleted).HasDefaultValue(false);
-        builder.HasIndex(x => x.SiteUrl).IsUnique();
+        builder.HasIndex(x => new { x.OwnerUserId, x.SiteUrl }).IsUnique();
+        builder.HasIndex(x => x.OwnerUserId);
         builder.HasQueryFilter(x => !x.IsDeleted);
     }
 }
