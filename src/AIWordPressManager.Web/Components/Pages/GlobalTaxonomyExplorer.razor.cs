da397bbe80e23755ef23d67df838397726c10fdf
walidatiyaai2025-gif <walidatiyaai2025@gmail.com>
2026-08-07 00:12:51 +0300

    feat: make taxonomy explorer workspace aware

---
+++ src/AIWordPressManager.Web/Components/Pages/GlobalTaxonomyExplorer.razor.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Components;
+
+namespace AIWordPressManager.Web.Components.Pages;
+
+public partial class GlobalTaxonomyExplorer
+{
+    private bool _workspaceQueryInitialized;
+
+    [Parameter]
+    [SupplyParameterFromQuery(Name = "siteId")]
+    public Guid? WorkspaceSiteId { get; set; }
+
+    protected override async Task OnParametersSetAsync()
+    {
+        var nextFilter = WorkspaceSiteId?.ToString() ?? string.Empty;
+        var changed = !string.Equals(_siteFilter, nextFilter, StringComparison.OrdinalIgnoreCase);
+        _siteFilter = nextFilter;
+
+        if (!_workspaceQueryInitialized)
+        {
+            _workspaceQueryInitialized = true;
+            return;
+        }
+
+        if (changed)
+        {
+            _page = 1;
+            await LoadAsync();
+        }
+    }
+}
