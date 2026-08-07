89b77be128263873aa7323722af4e4fcb3f8d536
walidatiyaai2025-gif <walidatiyaai2025@gmail.com>
2026-08-07 04:13:54 +0300

    fix: defer users workspace query until owned sites load

--- src/AIWordPressManager.Web/Components/Pages/GlobalUsersWorkspace.razor.cs
+++ src/AIWordPressManager.Web/Components/Pages/GlobalUsersWorkspace.razor.cs
@@ -5,6 +5,7 @@ namespace AIWordPressManager.Web.Components.Pages;
 public partial class GlobalUsersWorkspace
 {
     private bool _workspaceQueryInitialized;
+    private Guid _pendingWorkspaceSiteId;
 
     [Parameter]
     [SupplyParameterFromQuery(Name = "siteId")]
@@ -12,7 +13,32 @@ public partial class GlobalUsersWorkspace
 
     protected override async Task OnParametersSetAsync()
     {
-        var nextSiteId = WorkspaceSiteId ?? Guid.Empty;
+        _pendingWorkspaceSiteId = WorkspaceSiteId ?? Guid.Empty;
+
+        if (_loadingSites)
+            return;
+
+        await ApplyWorkspaceQueryAsync();
+    }
+
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        await base.OnAfterRenderAsync(firstRender);
+
+        if (_loadingSites || _workspaceQueryInitialized)
+            return;
+
+        await ApplyWorkspaceQueryAsync();
+        await InvokeAsync(StateHasChanged);
+    }
+
+    private async Task ApplyWorkspaceQueryAsync()
+    {
+        var nextSiteId = _pendingWorkspaceSiteId != Guid.Empty &&
+                         _sites.Any(site => site.Id == _pendingWorkspaceSiteId)
+            ? _pendingWorkspaceSiteId
+            : Guid.Empty;
+
         var changed = _selectedSiteId != nextSiteId;
         _selectedSiteId = nextSiteId;
 
