5192a785286f2edeedd6d1da6292752fce1db8a7
walidatiyaai2025-gif <walidatiyaai2025@gmail.com>
2026-08-09 12:20:59 +0300

    test: cover anonymous root routing

---
+++ tests/AIWordPressManager.Tests/PublicEntryRoutingTests.cs
@@ -0,0 +1,29 @@
+using AIWordPressManager.Web.Services;
+using FluentAssertions;
+
+namespace AIWordPressManager.Tests;
+
+public sealed class PublicEntryRoutingTests
+{
+    [Theory]
+    [InlineData("/", "GET", false, true)]
+    [InlineData("/", "HEAD", false, true)]
+    [InlineData("/", "GET", true, false)]
+    [InlineData("/welcome", "GET", false, false)]
+    [InlineData("/", "POST", false, false)]
+    public void ShouldRedirectToLanding_AppliesOnlyToAnonymousRootReads(
+        string path,
+        string method,
+        bool isAuthenticated,
+        bool expected)
+    {
+        PublicEntryRouting.ShouldRedirectToLanding(path, method, isAuthenticated)
+            .Should().Be(expected);
+    }
+
+    [Fact]
+    public void LandingPath_Remains_PublicWelcome()
+    {
+        PublicEntryRouting.LandingPath.Should().Be("/welcome");
+    }
+}
