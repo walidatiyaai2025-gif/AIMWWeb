9eebba20f789c3767b96df2f55ba0e3e34a1ddc2
walidatiyaai2025-gif <walidatiyaai2025@gmail.com>
2026-08-11 16:24:16 +0300

    UX-010 restore DbContext metadata on SEO snapshot migration

--- src/AIWordPressManager.Persistence/Migrations/20260803110000_AddSeoAuditSnapshots.cs
+++ src/AIWordPressManager.Persistence/Migrations/20260803110000_AddSeoAuditSnapshots.cs
@@ -1,8 +1,10 @@
+using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 namespace AIWordPressManager.Persistence.Migrations;
 
+[DbContext(typeof(AppDbContext))]
 [Migration("20260803110000_AddSeoAuditSnapshots")]
 public partial class AddSeoAuditSnapshots : Migration
 {
