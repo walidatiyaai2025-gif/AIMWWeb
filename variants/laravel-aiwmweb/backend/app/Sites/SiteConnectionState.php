2d4822979b6df890ea6a7b30cd27f24012d6c2e0
walidatiyaai2025-gif <walidatiyaai2025@gmail.com>
2026-08-28 00:06:13 +0300

    fix(sites): satisfy Laravel formatting

--- variants/laravel-aiwmweb/backend/app/Sites/SiteConnectionState.php
+++ variants/laravel-aiwmweb/backend/app/Sites/SiteConnectionState.php
@@ -5,12 +5,19 @@ namespace App\Sites;
 final class SiteConnectionState
 {
     public const CONNECTED = 'CONNECTED';
+
     public const DEGRADED = 'DEGRADED';
+
     public const DISCONNECTED = 'DISCONNECTED';
+
     public const AUTH_FAILED = 'AUTH_FAILED';
+
     public const CONNECTOR_DISABLED = 'CONNECTOR_DISABLED';
+
     public const CAPABILITY_DISABLED = 'CAPABILITY_DISABLED';
+
     public const UNSUPPORTED = 'UNSUPPORTED';
+
     public const TEMPORARILY_UNAVAILABLE = 'TEMPORARILY_UNAVAILABLE';
 
     public const ALL = [
