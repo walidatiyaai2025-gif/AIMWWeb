eca9556278bfb3744173f27a902105ebc0277b18
walidatiyaai2025-gif <walidatiyaai2025@gmail.com>
2026-08-27 23:54:25 +0300

    feat(seo): cast persisted metadata state

--- variants/laravel-aiwmweb/backend/app/Models/SyncedContent.php
+++ variants/laravel-aiwmweb/backend/app/Models/SyncedContent.php
@@ -6,6 +6,12 @@ class SyncedContent extends DomainModel
 {
     protected function casts(): array
     {
-        return ['headings' => 'array', 'taxonomy' => 'array', 'media' => 'array', 'remote_modified_at' => 'datetime'];
+        return [
+            'headings' => 'array',
+            'taxonomy' => 'array',
+            'media' => 'array',
+            'seo_robots' => 'array',
+            'remote_modified_at' => 'datetime',
+        ];
     }
 }
