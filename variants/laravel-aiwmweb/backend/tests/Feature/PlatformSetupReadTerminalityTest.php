0081cf664139ab0e7645436ec2f8fd3c39219c87
walidatiyaai2025-gif <walidatiyaai2025@gmail.com>
2026-08-29 00:22:17 +0300

    test: match Laravel invokable route action name

--- variants/laravel-aiwmweb/backend/tests/Feature/PlatformSetupReadTerminalityTest.php
+++ variants/laravel-aiwmweb/backend/tests/Feature/PlatformSetupReadTerminalityTest.php
@@ -19,7 +19,7 @@ class PlatformSetupReadTerminalityTest extends TestCase
         $route = Route::getRoutes()->getByName('canonical.api.setup');
 
         $this->assertNotNull($route);
-        $this->assertSame(SetupReadController::class.'@__invoke', $route->getActionName());
+        $this->assertSame(SetupReadController::class, $route->getActionName());
         $this->assertContains('GET', $route->methods());
         $this->assertContains('web', $route->gatherMiddleware());
         $this->assertNotContains('auth', $route->gatherMiddleware());
