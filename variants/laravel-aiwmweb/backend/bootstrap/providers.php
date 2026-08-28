5b509fe48e0cce76c892753d7f06cede77ebcc75
walidatiyaai2025-gif <walidatiyaai2025@gmail.com>
2026-08-29 01:11:37 +0300

    style(laravel): order approval route provider import

--- variants/laravel-aiwmweb/backend/bootstrap/providers.php
+++ variants/laravel-aiwmweb/backend/bootstrap/providers.php
@@ -1,7 +1,7 @@
 <?php
 
-use App\Providers\AppServiceProvider;
 use App\Providers\ApprovalQueueRouteServiceProvider;
+use App\Providers\AppServiceProvider;
 use App\Providers\SetupRouteServiceProvider;
 
 return [
