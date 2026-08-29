22590863a558fba95bef8348f41e881950f3d90d
walidatiyaai2025-gif <walidatiyaai2025@gmail.com>
2026-08-29 11:31:55 +0300

    feat(laravel): enable approvals report export routes

--- variants/laravel-aiwmweb/backend/bootstrap/providers.php
+++ variants/laravel-aiwmweb/backend/bootstrap/providers.php
@@ -1,7 +1,9 @@
 <?php
 
 use App\Providers\AppServiceProvider;
+use App\Providers\ApprovalsReportExportRouteServiceProvider;
 
 return [
     AppServiceProvider::class,
+    ApprovalsReportExportRouteServiceProvider::class,
 ];
